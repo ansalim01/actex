667d98896a1411e2ee38af6dbcd24d47836ed894
ansalim01 <ansalim14@gmail.com>
2023-06-12 03:59:25 +0300

    hh

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
 
   <BrowserRouter basename="actex">
-    <App />
+    <Provider>
+
+      <App />
+    </Provider>
   </BrowserRouter>
   // </React.StrictMode>
 );
