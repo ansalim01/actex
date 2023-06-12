03b0a7aae2e9d51e80da2ccf96bd0c2d7a2029e5
ansalim01 <ansalim14@gmail.com>
2023-06-12 03:22:26 +0300

    ее

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path={'/'} element={<UsersAdm />} />
+        <Route index path={'/'} element={<UsersAdm />} />
       </Routes>
 
     </div>
