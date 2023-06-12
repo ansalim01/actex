7c21fe747ddc1a7f75124b324b120211302ae7bc
ansalim01 <ansalim14@gmail.com>
2023-06-12 03:44:42 +0300

    5

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <div>
+      text
       <Routes basename="actex">
         {/* <HashRouter basename="actex"> */}
         <Route exact path={'/'} element={<UsersAdm />} />
